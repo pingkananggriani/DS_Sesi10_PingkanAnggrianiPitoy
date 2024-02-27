1f2df888abfb5887d5a115f847c8f18cf7387efd
Pingkan <pingkananggriani@gmail.com>
2024-02-27 23:49:49 +0800

    ds10

---
+++ wdio.conf.js
@@ -0,0 +1,35 @@
+require('dotenv/config')
+
+exports.config = {
+    runner: 'local',
+    specs: [
+        './features/**/*.feature'
+    ],
+    maxInstances: 10,
+    capabilities: [{
+        browserName: 'chrome'
+    }],
+    logLevel: 'info',
+    bail: 0,
+    baseUrl: 'https://www.saucedemo.com',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    services: ['devtools'],
+    framework: 'cucumber',
+    reporters: [['allure', {outputDir: 'allure-results'}]],
+    cucumberOpts: {
+        require: ['./features/step-definitions/steps.js'],
+        backtrace: false,
+        requireModule: [],
+        dryRun: false,
+        failFast: false,
+        name: [],
+        snippets: true,
+        source: true,
+        strict: false,
+        tagExpression: '',
+        timeout: 60000,
+        ignoreUndefinedDefinitions: false
+    },
+}
